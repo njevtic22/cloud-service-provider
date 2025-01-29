aa53b6c3c5f8ed077cdbe9579ce832ce274c00a4
Nemanja Jevtić <njevtic22@gmail.com>
2025-01-29 23:20:49 +0100

    Implemented getting user by id

--- cloud-api/cloud/src/main/java/com/demo/cloud/controller/UserController.java
+++ cloud-api/cloud/src/main/java/com/demo/cloud/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +56,12 @@ public class UserController {
                 users.getTotalElements(),
                 users.getTotalPages()
         ));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserViewDto> getById(@PathVariable Long id) {
+        User found = service.getById(id);
+        UserViewDto foundDto = mapper.toViewDto(found);
+        return ResponseEntity.ok(foundDto);
     }
 }
--- cloud-api/cloud/src/main/java/com/demo/cloud/mapper/UserMapper.java
+++ cloud-api/cloud/src/main/java/com/demo/cloud/mapper/UserMapper.java
@@ -2,19 +2,22 @@ package com.demo.cloud.mapper;
 
 import com.demo.cloud.dto.user.AddUserDto;
 import com.demo.cloud.dto.user.UserViewDto;
+import com.demo.cloud.model.Organization;
 import com.demo.cloud.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
     public UserViewDto toViewDto(User user) {
+        Organization org = user.getOrganization();
         return new UserViewDto(
                 user.getId(),
                 user.getName(),
                 user.getSurname(),
                 user.getEmail(),
                 user.getUsername(),
-                user.getRole().getName()
+                user.getRole().getName(),
+                org == null ? null : org.getId()
         );
     }
 
