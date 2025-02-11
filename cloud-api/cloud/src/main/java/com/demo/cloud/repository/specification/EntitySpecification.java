6295a1d9900aaa4e2941ffb76d16d46d95763ef1
Nemanja Jevtić <njevtic22@gmail.com>
2025-02-12 00:16:29 +0100

    Code refactoring

--- cloud-api/cloud/src/main/java/com/demo/cloud/repository/specification/EntitySpecification.java
+++ cloud-api/cloud/src/main/java/com/demo/cloud/repository/specification/EntitySpecification.java
@@ -1,5 +1,8 @@
 package com.demo.cloud.repository.specification;
 
+import com.demo.cloud.model.VirtualMachine;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.Map;
@@ -10,21 +13,59 @@ public class EntitySpecification {
         return filter;
     }
 
-    public static <T> Specification<T> attrLike(String attr, String keyword) {
-        if (keyword == null || keyword.isBlank()) {
-            return null;
-        }
-
-        return (root, query, cb) -> cb.like(cb.upper(root.get(attr)), "%" + keyword.toUpperCase() + "%");
+    public static <T> Specification<T> attrLike(String key, String value) {
+        return attrLike(new String[]{key}, value);
     }
 
-    public static <T> Specification<T> attrEqual(String attr, String value) {
+    public static <T> Specification<T> attrLike(String[] keys, String value) {
         if (value == null || value.isBlank()) {
             return null;
         }
 
-        boolean archived = Boolean.parseBoolean(value);
+        return (root, query, cb) -> cb.like(cb.upper(getPath(root, keys)), "%" + value.toUpperCase() + "%");
+    }
 
-        return (root, query, cb) -> cb.equal(root.get(attr), archived);
+    public static <T> Specification<T> attrEqual(String key, Object value) {
+        return attrEqual(new String[]{key}, value);
+    }
+
+    public static <T> Specification<T> attrEqual(String[] keys, Object value) {
+        if (value == null) {
+            return null;
+        }
+
+        return (root, query, cb) -> cb.equal(getPath(root, keys), value);
+    }
+
+    protected static Specification<VirtualMachine> attrMax(String key, String value) {
+        return attrMax(new String[]{key}, value);
+    }
+
+    protected static Specification<VirtualMachine> attrMax(String[] keys, String value) {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+
+        return (root, query, cb) -> cb.lessThanOrEqualTo(getPath(root, keys), value);
+    }
+
+    protected static Specification<VirtualMachine> attrMin(String key, String value) {
+        return attrMin(new String[]{key}, value);
+    }
+
+    protected static Specification<VirtualMachine> attrMin(String[] keys, String value) {
+        if (value == null || value.isBlank()) {
+            return null;
+        }
+
+        return (root, query, cb) -> cb.greaterThanOrEqualTo(getPath(root, keys), value);
+    }
+
+    protected static <T> Path<String> getPath(Root<T> root, String... keys) {
+        Path<String> path = root.get(keys[0]);
+        for (int i = 1; i < keys.length; i++) {
+            path = path.get(keys[i]);
+        }
+        return path;
     }
 }
--- cloud-api/cloud/src/main/java/com/demo/cloud/repository/specification/MachineSpecification.java
+++ cloud-api/cloud/src/main/java/com/demo/cloud/repository/specification/MachineSpecification.java
@@ -1,8 +1,6 @@
 package com.demo.cloud.repository.specification;
 
 import com.demo.cloud.model.VirtualMachine;
-import jakarta.persistence.criteria.Path;
-import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
@@ -31,8 +29,8 @@ public class MachineSpecification extends EntitySpecification {
                 case "maxRam" -> attrMax(new String[]{"category", "ram"}, value);
                 case "maxGpu" -> attrMax(new String[]{"category", "gpu"}, value);
 
-                case "organizationId" -> orgIdEqual(value);
-                case "archived" -> attrEqual(key, value);
+                case "organizationId" -> attrEqual(new String[]{"organization", "id"}, value);
+                case "archived" -> attrEqual(key, Boolean.valueOf(value));
                 default -> throw new IllegalArgumentException("Invalid filter key " + key);
             };
 
@@ -45,39 +43,5 @@ public class MachineSpecification extends EntitySpecification {
                 .stream()
                 .reduce(Specification::and)
                 .orElse(null);
-    }
-
-    public static Specification<VirtualMachine> orgIdEqual(String strId) {
-        if (strId == null || strId.isBlank()) {
-            return null;
-        }
-
-        Long orgId = Long.valueOf(strId);
-
-        return (root, query, cb) -> cb.equal(root.get("organization").get("id"), orgId);
-    }
-
-    private static Specification<VirtualMachine> attrMin(String[] keys, String value) {
-        if (value == null || value.isBlank()) {
-            return null;
-        }
-
-        return (root, query, cb) -> cb.greaterThanOrEqualTo(getPath(root, keys), value);
-    }
-
-    private static Specification<VirtualMachine> attrMax(String[] keys, String value) {
-        if (value == null || value.isBlank()) {
-            return null;
-        }
-
-        return (root, query, cb) -> cb.lessThanOrEqualTo(getPath(root, keys), value);
-    }
-
-    private static Path<String> getPath(Root<VirtualMachine> root, String... keys) {
-        Path<String> path = root.get(keys[0]);
-        for (int i = 1; i < keys.length; i++) {
-            path = path.get(keys[i]);
-        }
-        return path;
     }
 }
