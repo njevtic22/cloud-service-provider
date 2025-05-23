7d7f1956adbfa599ba91a7c0cf6c81c150b3e626
Nemanja Jevtić <njevtic22@gmail.com>
2025-05-23 17:50:15 +0200

    Added not found image

--- cloud-ui/src/pages/NotFoundPage.vue
+++ cloud-ui/src/pages/NotFoundPage.vue
@@ -1,7 +1,25 @@
 <template>
-    <h1>Not Found</h1>
+    <div>
+        <v-row align="stretch" class="justify-center">
+            <v-col cols="4">
+                <v-img
+                    src="@/assets/not-found.png"
+                    error="Even image is not found"
+                />
+            </v-col>
+        </v-row>
+        <v-row align="stretch" class="justify-center">
+            <v-col cols="5" class="d-flex justify-center">
+                <h1 class="not-found-text">Resource not found</h1>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+.not-found-text {
+    color: #b71c1c;
+}
+</style>
