894b4fdfd24477f3a7283bb449cd5180158c1b56
Nemanja Jevtić <njevtic22@gmail.com>
2025-06-27 12:57:46 +0200

    Added empty form

--- cloud-ui/src/components/UserForm.vue
+++ cloud-ui/src/components/UserForm.vue
@@ -1,7 +1,72 @@
 <template>
     <v-card :append-icon="icon" :title="title">
         <v-card-text>
-            <h1>This is card</h1>
+            <v-form ref="form">
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.name"
+                            :rules="[rules.required]"
+                            label="Name"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.surname"
+                            :rules="[rules.required]"
+                            label="Surname"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.email"
+                            :rules="[rules.required, rules.email]"
+                            label="Email"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.username"
+                            :rules="[rules.required]"
+                            label="Username"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.organization"
+                            :rules="[rules.required]"
+                            label="Organization"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.role"
+                            :rules="[rules.required]"
+                            label="Role"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.password"
+                            :rules="[rules.required]"
+                            label="Password"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <v-text-field
+                            v-model="user.repeatedPassword"
+                            :rules="[rules.required]"
+                            label="Repeated password"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-form>
         </v-card-text>
 
         <v-card-actions>
@@ -16,6 +81,8 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 defineProps({
     icon: String,
     title: String,
@@ -27,6 +94,27 @@ defineProps({
 });
 
 const emit = defineEmits(["submit", "cancel"]);
+const form = ref(null);
+
+const user = ref({
+    name: "",
+    surname: "",
+    email: "",
+    username: "",
+    organization: -1,
+    role: "",
+    password: "",
+    repeatedPassword: "",
+});
+
+const rules = {
+    required: (value) => Boolean(value) || "Required",
+    email: (email) =>
+        /.+@.+\..+/.test(email) || "Email must be valid email adress",
+    matchWithNewPassword: (value) =>
+        value === data.value.newPassword ||
+        "Repeated password does not match new password",
+};
 
 function clear() {
     console.log("Called clear");
