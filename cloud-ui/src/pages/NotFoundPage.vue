b908b1b63d908e87c26aea0196761ba1958534f5
Nemanja Jevtić <njevtic22@gmail.com>
2025-07-15 17:05:38 +0200

    Changed not found image

--- cloud-ui/src/pages/NotFoundPage.vue
+++ cloud-ui/src/pages/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <v-row align="stretch" class="justify-center">
+            <!-- In future, try to make image responsive with CSS selector @media linke in ProfileData.vue -->
             <v-col cols="8" xs="8" sm="5" md="4" lg="4" xl="4" xxl="4">
                 <v-img
                     src="@/assets/not-found.png"
